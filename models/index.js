993162fb9d84f6894316b44c6d2bad6a44f79862
Sadie Sial <sadiesial@gmail.com>
2021-10-25 22:05:05 -0500

    defining assoc with categories

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Category.belongsToMany(Product, {
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Category.belongsToMany(Product)
+
 
 module.exports = {
   Product,
