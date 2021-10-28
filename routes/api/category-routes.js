38fe3bc4dc6f887ca83537a8e75916a3f6f91dc2
Sadie Sial <sadiesial@gmail.com>
2021-10-27 20:14:03 -0500

    added 400 + 500 errors

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,30 +7,58 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories - findAll grabs from the whole db table
   // be sure to include its associated Products
-    Category.findAll({
-      include: [
-        {
-          model: Product,
-          attributes: ['id', 'product_id', 'product_name', 'price', 'stock']
-        }
-      ]
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_id', 'product_name', 'price', 'stock'],
+      }
+    ]
+  })
+    .then((categoryData) => res.json(categoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     })
-    .then(categoryData => res.json(categoryData))
-    .catch(err => {
-      console.log(err)
-      res.status(500).json(err)
-    });
-  });
+});
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value - findByPk Student Act 6 - 'GET a 'single' x by its primary key'
+  // find one category by its `id` value - findOne Student Act 6 - 'GET a 'single' x by its primary key'
   // be sure to include its associated Products
-  
-
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+        include: {
+          //Get product tag
+          model: Tag,
+          attributes: ['tag_name'],
+          through: ProductTag,
+        },
+      },
+    ],
+  })
+  // search db for id matching catagory and if nothing is found....
+    .then((categoryData) => {
+      if (!categoryData) {
+        res.status(404).json({ message: 'There is no category associated with this id, please try again.' });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
