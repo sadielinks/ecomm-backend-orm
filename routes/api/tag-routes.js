9cce49cb5d9cb8d7d2f8814ab2669d26d78b348d
Sadie Sial <sadiesial@gmail.com>
2021-10-27 21:14:11 -0500

    router.get by single tag added in

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,6 +6,21 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [
+      // Product data
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+        through: ProductTag,
+      }
+    ]
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  });
 });
 
 router.get('/:id', (req, res) => {
