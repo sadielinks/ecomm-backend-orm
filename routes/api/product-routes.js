7e922aa95b1f68d25c2ab95696bc9b4a186477c9
Sadie Sial <sadiesial@gmail.com>
2021-10-27 21:08:12 -0500

    router.delete + error messages

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -142,6 +142,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({message: 'There is no product associated with this id, please try again.'})
+      return;
+    }
+    res.json(categoryData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
