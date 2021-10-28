c003345a56b79a2747024a6ab2bb83e82e174700
Sadie Sial <sadiesial@gmail.com>
2021-10-27 20:19:02 -0500

    then + catch added to PUT

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -68,6 +68,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({message: 'There is no category associated with this id, please try again.'})
+      return;
+    }
+    res.json(categoryData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
